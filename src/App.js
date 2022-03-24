e09b3689e75c529139e4df5e8f15f9f5e4846f09
SyominAlex <gurufx.43@gmail.com>
2022-03-24 19:29:56 +0300

    homework1 done!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Message } from "./components/Message";
+
+const name = "Syomin Alex";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const foo = () => {
+        alert("Hello");
+    };
+    return (
+        <div className="App">
+            <Message name={name} asd={30 + 2} doSmth={foo} bold={true} />
+            <Message name="My Best Friend" asd={30 + 10} doSmth={foo} />
+        </div>
+    );
 }
 
 export default App;
