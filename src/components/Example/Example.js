2b9d29a23bdb20a34325ed4a50e91091fd09e0b8
SyominAlex <gurufx.43@gmail.com>
2022-03-25 20:17:21 +0300

    setState() example on class component

--- src/components/Example/Example.js
+++ src/components/Example/Example.js
@@ -1,15 +1,35 @@
-import { useState } from "react";
+/* Пример на функциональном компоненте */
 
-export const Counter = () => {
-    // let count = 0;
-    const [count, setCount] = useState(0);
-    // const countState = useState(0);
-    // const count = countState[0];
-    // const setCount = countState[1];
-    return (
-        <div>
-            <h4>{count}</h4>
-            <button onClick={() => setCount(count + 1)}>Click!</button>
-        </div>
-    );
-};
+// import { useState } from "react";
+//
+// export const Counter = () => {
+//     // let count = 0;
+//     const [count, setCount] = useState(0);
+//     // const countState = useState(0);
+//     // const count = countState[0];
+//     // const setCount = countState[1];
+//     return (
+//         <div>
+//             <h4>{count}</h4>
+//             <button onClick={() => setCount(count + 1)}>Click!</button>
+//         </div>
+//     );
+// };
+
+/* Пример на классовом компоненте */
+
+import React from "react";
+
+export class Counter extends React.Component {
+    state = {
+        count: 0,
+    };
+    render() {
+        return (
+            <div>
+                <h4>{this.state.count}</h4>
+                <button onClick={() => this.setState({count: this.state.count + 1})}>Click!</button>
+            </div>
+        );
+    }
+}
