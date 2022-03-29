764e1a29a44ad1e1149efd2b0d93ed5ff77fbe47
SyominAlex <gurufx.43@gmail.com>
2022-03-29 06:09:32 +0300

    useEffect() examples

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import "./App.css";
-import React from "react";
-import { Message } from "./components/Message";
+import React, {useState} from "react";
+import { Message } from "./components/Message/Message";
 import { Counter } from "./components/Example/Example";
 
 const name = "Syomin Alex";
 
 function App() {
+    const [rand, setRand] = useState(0); // пример удобства использования useEffect()
+
+    const updateRand = () => {
+        setRand(Math.random());
+    };
+
     const foo = () => {
         alert("Hello");
     };
     return (
         <div className="App">
-            <Counter />
+            <Counter randomNumber={rand} />
+            <button onClick={updateRand}>Update random</button>
             <Message name={name} age={30 + 2} doSmth={foo} bold={true} />
             <Message name="My Best Friend" age={30 + 10} doSmth={foo} />
         </div>
