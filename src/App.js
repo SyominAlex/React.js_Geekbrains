c667bcd7f7966ff217c51fbfccd5149c940febc9
SyominAlex <gurufx.43@gmail.com>
2022-04-11 00:04:58 +0300

    NavLink style + may be className

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import {BrowserRouter, Route, Routes, Link, NavLink} from "react-router-dom";
 import { Chat } from "./screens/Chat/Chat";
 
 const Home = () => <h4>Home page</h4>;
@@ -8,8 +8,14 @@ const Home = () => <h4>Home page</h4>;
 function App() {
     return (
         <BrowserRouter>
-            <Link to="/">Home</Link>
-            <Link to="/chat">Chat</Link>
+            <ul>
+                <li>
+                    <NavLink to="/" style={ ({ isActive }) => ({ color: isActive ? "green" : "blue" }) }>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/chat" style={ ({ isActive }) => ({ color: isActive ? "green" : "blue" }) }>Chat</NavLink>
+                </li>
+            </ul>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/chat" element={<Chat />} />
