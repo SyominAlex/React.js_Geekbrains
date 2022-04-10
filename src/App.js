c61015fc986affabd930c459df9aab5b4dbd84f8
SyominAlex <gurufx.43@gmail.com>
2022-04-10 10:25:41 +0300

    prepare for React-router-dom

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import "./App.css";
 import React, {useEffect, useRef, useState} from "react";
 import { MessageList } from "./components/MessageList/MessageList";
 import { Form } from "./components/Form/Form";
-import { ExampleForm } from "./components/Example/Example";
 import {AUTHORS, CHATS} from "./utils/constants";
 import {ChatList} from "./components/ChatList/ChatList";
-import {TextField} from "@mui/material";
 
 function App() {
 
@@ -48,37 +46,6 @@ function App() {
                     <MessageList messageList={messageList} />
                     <Form onSubmit={sendMessage} />
                 </div>
-                {/*паттерн render prop - вместо чилдренов используется рендер render={}*/}
-                <ExampleForm
-                    onSubmit={() => {}}
-                    render={({ value, handleChange }) => (
-                    <TextField
-                        style={{ margin: '20px' }}
-                        id="outlined-basic"
-                        label="Сообщение"
-                        variant="outlined"
-                        value={value}
-                        onChange={handleChange}
-                        // inputRef={inputRef}
-                    />
-                )}>
-                </ExampleForm>
-                {/* мы можем передать сюда любой текстовый input, для формы не важно какой, она будет эту логику применять,
-                какой бы мы инпут сюда ни передали, даже обычный html input, можем передавать начальные значения initialValues, массив или объект values,
-                для каждого инпута находилось бы свое value. Родительский компонент решает какие будут значения, он может проигнорировать какие-то из этих пропсов.
-                 Такой подход применяется чаще всего в формах, например, набирающая популярность библиотека react final form или formic (вся логика на стороне этой библиотеки). */}
-                <ExampleForm
-                    onSubmit={() => {}}
-                    render={({ value, handleChange }) => (
-                        <input
-                            style={{ margin: '20px' }}
-                            id="outlined-basic"
-                            value={value}
-                            onChange={handleChange}
-                        />
-                    )}
-                >
-                </ExampleForm>
             </div>
     );
 }
