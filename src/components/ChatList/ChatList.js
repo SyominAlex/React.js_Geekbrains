3a981073aaedc81bea13fea96c029f55fd7c0a5d
SyominAlex <gurufx.43@gmail.com>
2022-04-11 00:37:11 +0300

    add links & routes to chats

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -3,13 +3,19 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import { Link } from "react-router-dom";
+
 
 export const ChatList = ({ chatList }) => (
     <List sx={{ maxWidth: 200, flexGrow: 1 }} >
         {chatList.map((chatList) => (
             <ListItem disablePadding key={chatList.id.toString()} >
                 <ListItemButton>
-                    <ListItemText primary={chatList.name} />
+                    <ListItemText primary={
+                        <Link to={`/chat/${chatList.id}`}>
+                            {chatList.name}
+                        </Link>
+                    } />
                 </ListItemButton>
             </ListItem>
         ))}
