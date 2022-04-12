f231898cbdfc854eff8540593ccb7d75179b4648
SyominAlex <gurufx.43@gmail.com>
2022-04-12 19:55:37 +0300

    errors with ChatList & Outlet fixed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import {BrowserRouter, Route, Routes, NavLink} from "react-router-dom";
 import { Chat } from "./screens/Chat/Chat";
 import { ChatList } from "./components/ChatList/ChatList";
+import {CHATS} from "./utils/constants";
 
 const Home = () => <h4>Home page</h4>;
 
@@ -17,12 +18,14 @@ function App() {
                     <NavLink to="/chat" style={ ({ isActive }) => ({ color: isActive ? "green" : "blue" }) }>Chat</NavLink>
                 </li>
             </ul>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/chat" element={<ChatList />}>
-                    <Route path=":id" element={<Chat />} />
-                </Route>
-            </Routes>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/chat" element={<ChatList chatList={CHATS} />}>
+                        <Route path=":id" element={<Chat />} />
+                    </Route>
+                </Routes>
+            </div>
         </BrowserRouter>
     );
 }
