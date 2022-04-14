1f72a20f4d6e19cb3a6dd565a27aa467c6cea50c
SyominAlex <gurufx.43@gmail.com>
2022-04-14 19:49:26 +0300

    prepare for add chat task

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
-import React from "react";
+import React, {useState} from "react";
 import {BrowserRouter, Route, Routes, NavLink} from "react-router-dom";
+import { Home } from "./screens/Home/Home";
 import { Chat } from "./screens/Chat/Chat";
 import { ChatList } from "./components/ChatList/ChatList";
-import {CHATS} from "./utils/constants";
-
-const Home = () => <h4>Home page</h4>;
+import {initialChats} from "./utils/constants";
 
 function App() {
+    const [chats, setChats] = useState(initialChats);
+
     return (
         <BrowserRouter>
             <ul>
@@ -21,7 +22,7 @@ function App() {
             <div className="App">
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/chat" element={<ChatList chatList={CHATS} />}>
+                    <Route path="/chat" element={<ChatList chatList={initialChats} />}>
                         <Route path=":id" element={<Chat />} />
                     </Route>
                     <Route path="*" element={<h4>404</h4>} />
