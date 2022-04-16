e31bd69d00b148ad95613b6b9679a5ee11e3c1ea
SyominAlex <gurufx.43@gmail.com>
2022-04-16 11:47:55 +0300

    move chats, initMessages, addMessage to App.js

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -5,20 +5,20 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import { Link, Outlet } from "react-router-dom";
 
-export const ChatList = ({ chatList }) => {
-    if (!chatList) {
+export const ChatList = ({ chats }) => {
+    if (!chats) {
         console.log('chatList is null');
         return null;
     }
     return (
         <>
             <List sx={{ maxWidth: 200, flexGrow: 1 }} >
-                {chatList.map((chatList) => (
-                    <ListItem disablePadding key={chatList.id} >
+                {chats.map((chats) => (
+                    <ListItem disablePadding key={chats.id} >
                         <ListItemButton>
                             <ListItemText primary={
-                                <Link to={`/chat/${chatList.id}`} >
-                                    {chatList.name}
+                                <Link to={`/chat/${chats.id}`} >
+                                    {chats.name}
                                 </Link>
                             } />
                         </ListItemButton>
