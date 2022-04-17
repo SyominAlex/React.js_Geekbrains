99b1f3ab1b89bd3a6b1bd068e4e56df09b5236bc
SyominAlex <gurufx.43@gmail.com>
2022-04-17 21:26:14 +0300

    Context.Provider example for theme

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,7 +1,12 @@
 import "./Message.styles.css";
 import PropTypes from 'prop-types';
+import {useContext} from "react";
+import {ThemeContext} from "../../utils/ThemeContext";
 
-export const Message = ({ author, text, theme }) => {
+export const Message = ({ author, text }) => {
+    const theme = useContext(ThemeContext);
+    console.log(theme);
+
     return (
         <div className="message">
             <span style={{ color: theme === "dark" ? "red" : "blue" }}>{author}: </span>
