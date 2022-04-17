dea2401923e1165de7a5337251bf5b926d44aba1
SyominAlex <gurufx.43@gmail.com>
2022-04-17 20:33:58 +0300

    added form to change name in store & fix checkbox name

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,24 +1,40 @@
 import {useDispatch, useSelector} from "react-redux";
-import {Button} from "@mui/material";
+// import {Button} from "@mui/material";
+
+import {Form} from "../../components/Form/Form";
 import {toggleCheckbox} from "../../store/profile/actions";
+import { SET_NAME } from "../../store/profile/actions";
+
 
 export const Profile = () => {
     const dispatch = useDispatch(); // dispatch - это результат вызова хука useDispatch, этот хук возвращает функцию dispatch
+
     const state = useSelector(state => state);
     // console.log(state);
+
     const handleClick = () => {
         dispatch(toggleCheckbox);
-    }
+    };
+
+    const handleSubmit = (text) => {
+        dispatch({
+            type: SET_NAME,
+            payload: text,
+        });
+    };
+
     return (
         <>
-            <h3>This is Profile
+            <div className="profile">
+                <h3>This is Profile</h3>
+                {/*<Button variant={"contained"} onClick={handleClick}>Change show name</Button>*/}
+                <div>
+                    <input type="checkbox" id="checkbox" onClick={handleClick}></input>
+                    <label htmlFor="checkbox">hide name</label>
+                </div>
                 {state.showName && <span>{state.name}</span>}
-            <Button variant={"contained"} onClick={handleClick}>Change show name</Button>
-            <div>
-                <input type="checkbox" id="checkbox" onClick={handleClick}></input>
-                <label htmlFor="checkbox">change show name</label>
+                <Form onSubmit={handleSubmit} />
             </div>
-            </h3>
         </>
     );
 }
