a5bf77862c859e810f66f2b0e65e2968e9d382f0
SyominAlex <gurufx.43@gmail.com>
2022-04-20 18:59:53 +0300

    higher order function example withThemeContext(Message) + React-Redux installation

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -3,13 +3,15 @@ import PropTypes from 'prop-types';
 import {useContext} from "react";
 import {ThemeContext} from "../../utils/ThemeContext";
 
-export const Message = ({ author, text }) => {
-    const { theme } = useContext(ThemeContext);
-    console.log(theme);
+// export const Message = ({ author, text, color = "red" }) => {
+export const Message = ({ author, text, theme }) => {
+    // const { theme } = useContext(ThemeContext);
+    // console.log(theme);
 
     return (
         <div className="message">
             <span style={{ color: theme === "dark" ? "red" : "blue" }}>{author}: </span>
+            {/*<span style={{ color }}>{author}: </span>*/}
             <span>{text}</span>
         </div>
     );
@@ -19,3 +21,20 @@ Message.propTypes = {
     author: PropTypes.string.isRequired,
     text: PropTypes.string,
 }
+
+const withBlueColor = (Component) => (props) => {
+    return <Component {...props} color="blue" />
+}
+
+const withGreenColor = (Component) => (props) => {
+    return <Component {...props} color="green" />
+}
+
+const withThemeContext = (Component) => (props) => {
+    const { theme } = useContext(ThemeContext);
+    return <Component {...props} theme={theme} />
+}
+
+export const MessageWithBlueColor = withBlueColor(Message);
+export const MessageWithGreenColor = withGreenColor(Message);
+export const MessageWithThemeContext = withThemeContext(Message);
--- src/components/MessageList/MessageList.js
+++ src/components/MessageList/MessageList.js
@@ -1,7 +1,10 @@
-import {Message} from "../Message/Message";
+import { Message, MessageWithBlueColor, MessageWithGreenColor, MessageWithThemeContext } from "../Message/Message";
 
 export const MessageList = ({ messages }) => (
     messages.map((msg) => (
-        <Message key={msg.id} text={msg.text} author={msg.author} />
+        // <Message key={msg.id} text={msg.text} author={msg.author} />
+        // <MessageWithBlueColor key={msg.id} text={msg.text} author={msg.author} />
+        // <MessageWithGreenColor key={msg.id} text={msg.text} author={msg.author} />
+        <MessageWithThemeContext key={msg.id} text={msg.text} author={msg.author} />
     ))
 );
