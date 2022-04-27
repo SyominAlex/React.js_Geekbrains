c338b523d3c45da42a966061ba96745287b48ff0
SyominAlex <gurufx.43@gmail.com>
2022-04-27 20:24:57 +0300

    selectors, reducer, actions for chats & messages

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import {combineReducers, createStore} from "redux";
 import {profileReducer} from "./profile/reducer";
 import {chatsReducer} from "./chats/reducer";
+import {messagesReducer} from "./messages/reducer";
 
 const rootReducer = combineReducers({
     profile: profileReducer,
     chats: chatsReducer,
+    messages: messagesReducer,
 });
 
 export const store = createStore(
