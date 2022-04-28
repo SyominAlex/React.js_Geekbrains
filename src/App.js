78bf019491ec650b0f539e6b492c627677147a31
SyominAlex <gurufx.43@gmail.com>
2022-04-28 20:59:31 +0300

    clear comments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-// import {shallowEqual, useDispatch, useSelector} from "react-redux";
 import {BrowserRouter, Route, Routes, NavLink} from "react-router-dom";
 
 import "./App.css";
@@ -10,55 +9,9 @@ import { Chat } from "./screens/Chat/Chat";
 import { ChatList } from "./components/ChatList/ChatList";
 import {ThemeContext} from "./utils/ThemeContext";
 import {Profile} from "./screens/Profile/Profile";
-// import { addChat, deleteChat } from "./store/chats/actions";
-// import {selectChats} from "./store/chats/selectors";
-// import {selectMessages} from "./store/messages/selectors";
-// import {addMessage, clearMessages, initMessagesForChat} from "./store/messages/actions";
-
-/*const initMessages = initialChats.reduce((acc, chat) => {
-    acc[chat.id] = [];
-    return acc;
-}, {});*/
 
 function App() {
-    // const [chats, setChats] = useState(initialChats); // не будем хранить их в стейте, а будем получать из стора
-    //     const chats = useSelector(selectChats, shallowEqual);
-    //     const messages = useSelector(selectMessages);
-    /* в такой записи оптимизация будет менее эффективна, чем в случае с профилем,
-    т.к. в профиле из селекторов возвращаем примитивы, а в случае с чатами всё время возвращаем новый объект.
-    Создатели редакса в курсе проблемы, поэтому дали нам возможность оптимизировать, useSelector имеет второй аргумент -
-    функцию сравнения: если аргументы не равны, пытается обновить.
-    - если используем в качестве селектора именованную константную функцию, где это возможно
-    - если возвращаем не примитив, стараемся использовать функцию сравнения shallowEqual
-    - прежде чем оптимизировать - делать замеры */
-    // const dispatch = useDispatch();
-    // const [messages, setMessages] = useState(initMessages);
     const [theme, setTheme] = useState('dark');
-
-    // const addNewMessage = (newMsg, id) => {
-    //     // setMessages({ ...messages, [id]: [...messages[id], newMsg] });
-    //     dispatch(addMessage(newMsg, id));
-    // };
-    //
-    // const addNewChat = (newChat) => {
-    //     // setChats((prevChats) => [...prevChats, newChat]);
-    //     dispatch(addChat(newChat));
-    //     dispatch(initMessagesForChat(newChat.id));
-    //     // setMessages((prevMessages) => ({ ...prevMessages, [newChat.id]: [] }));
-    // };
-    //
-    // const removeChat = (id) => {
-    //     // setChats((prevChats) => prevChats.filter((chat) => chat.id !== id));
-    //     dispatch(deleteChat(id));
-    //     dispatch(clearMessages(id));
-    //     // setMessages((prevMessages) => {
-    //     //     const newMessages = {...prevMessages};
-    //     //     delete newMessages[id];
-    //     //
-    //     //     return newMessages;
-    //     // });
-    // };
-
     // в итоге App содержит только тему (для демонстрационных целей) и роутинг
     const toggleLinkStyle = ({ isActive }) => ({ color: isActive ? "green" : "blue" });
 
@@ -69,12 +22,6 @@ function App() {
     return (
             <ThemeContext.Provider value={{ theme, changeTheme: toggleTheme }}>
                 <BrowserRouter>
-                    {/*Для чего нужен context: эта кнопка и Message не связаны общим родителем, нужно передать данные в компонент Message*/}
-                    {/*<Button className={"Button"}  variant={"outlined"} onClick={() => setTheme(
-                    (prevTheme) => (prevTheme === "dark" ? "light" : "dark")
-                )}>
-                    Сменить тему
-                </Button>*/}
                     <ul>
                         <li>
                             <NavLink to="/" style={ toggleLinkStyle }>Home</NavLink>
@@ -90,8 +37,8 @@ function App() {
                         <Routes>
                             <Route path="/" element={<Home />} />
                             <Route path="/profile" element={<Profile />} />
-                            <Route path="/chat" element={<ChatList />}> {/*chats={chats} addChat={addNewChat} deleteChat={removeChat}*/}
-                                <Route path=":id" element={<Chat />} /> {/*messages={messages} addMessage={addNewMessage}*/}
+                            <Route path="/chat" element={<ChatList />}>
+                                <Route path=":id" element={<Chat />} />
                             </Route>
                             <Route path="*" element={<h4>404</h4>} />
                         </Routes>
