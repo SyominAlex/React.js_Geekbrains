4e60f88ed3ee00f30b5851708c2cf711fa8632e0
SyominAlex <gurufx.43@gmail.com>
2022-05-03 20:12:54 +0300

    prevName in Profile component

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,21 +1,28 @@
-import {/*connect, */useDispatch, useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
 import {Form} from "../../components/Form/Form";
 import {setName, toggleCheckbox} from "../../store/profile/actions";
 import {selectName, selectShowName} from "../../store/profile/selectors";
+import {useEffect, useRef} from "react";
 
 export const Profile = () => {
-    const dispatch = useDispatch(); // dispatch - это результат вызова хука useDispatch, который возвращает функцию dispatch
-    // const state = useSelector(state => state);
-    // возвращаем не объект, а примитивы, их сравнивать проще
-    // - с useSelector легче читать стало, понятно каким селектором что забираем, не надо разбираться во внутренней структуре стора
-    // - у нас постоянная ссылка на эту функцию независимо от того в каком рендере находимся, что помогает редаксу понять, что лишний рендер выполнять не надо
+    const dispatch = useDispatch();
     const name = useSelector(selectName);
     const showName = useSelector(selectShowName);
+    const prevName = useRef(); // изначально = undefined
 
     const handleClick = () => {
         dispatch(toggleCheckbox);
     };
+
+    useEffect(() => {
+        prevName.current = name;
+    }, [name]);
+    /* После изменения name происходит обновление стейта за счет нового пропса или useSelector, происходит рендер этого компонента, вызов этой всей функции.
+    * После того как рендер произошел - отрабатывают useEffect (его вызов происходит после return).
+    * Т.е. произошел рендер с обновленным name, а после этого в предыдущее значение сохраняется name. */
+
+    console.log(prevName.current);
 
     const handleSubmit = (text) => {
         dispatch(setName(text));
@@ -35,40 +42,3 @@ export const Profile = () => {
         </>
     );
 }
-
-// const ProfileToConnect = ({ name, showName, changeName, changeCheckbox }) => {
-//     // console.log(name, showName, changeName, changeCheckbox);
-//     const handleClick = () => {
-//         changeCheckbox();
-//     };
-//
-//     const handleSubmit = (text) => {
-//         changeName(text);
-//     };
-//
-//     return (
-//         <>
-//             <div className="profile">
-//                 <h3>This is Profile</h3>
-//                 <div>
-//                     <input type="checkbox" id="checkbox" onClick={handleClick}></input>
-//                     <label htmlFor="checkbox">show name</label>
-//                 </div>
-//                 {showName && <span>{name}</span>}
-//                 <Form onSubmit={handleSubmit} />
-//             </div>
-//         </>
-//     );
-// }
-//
-// const mapStateToProps = (state) => ({
-//     name: state.profile.name,
-//     showName: state.profile.showName,
-// });
-//
-// const mapDispatchToProps = {
-//     changeName: setName,
-//     changeCheckbox: () => toggleCheckbox,
-// };
-//
-// export const Profile = connect(mapStateToProps, mapDispatchToProps)(ProfileToConnect);
