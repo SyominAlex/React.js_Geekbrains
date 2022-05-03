6da21f425a255b89b13f47819eea51b93b00e1b9
SyominAlex <gurufx.43@gmail.com>
2022-05-03 20:12:54 +0300

    prevName in Profile component

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,15 +1,44 @@
-import {FormContainer} from "../../components/Form/FormContainer";
+import {useDispatch, useSelector} from "react-redux";
 
-export const Profile = ({ name, showName, handleClick, handleSubmit }) => (
-    <>
-        <div className="profile">
-            <h3>This is Profile</h3>
-            <div>
-                <input type="checkbox" id="checkbox" onClick={handleClick}></input>
-                <label htmlFor="checkbox">show name</label>
+import {Form} from "../../components/Form/Form";
+import {setName, toggleCheckbox} from "../../store/profile/actions";
+import {selectName, selectShowName} from "../../store/profile/selectors";
+import {useEffect, useRef} from "react";
+
+export const Profile = () => {
+    const dispatch = useDispatch();
+    const name = useSelector(selectName);
+    const showName = useSelector(selectShowName);
+    const prevName = useRef(); // изначально = undefined
+
+    const handleClick = () => {
+        dispatch(toggleCheckbox);
+    };
+
+    useEffect(() => {
+        prevName.current = name;
+    }, [name]);
+    /* После изменения name происходит обновление стейта за счет нового пропса или useSelector, происходит рендер этого компонента, вызов этой всей функции.
+    * После того как рендер произошел - отрабатывают useEffect (его вызов происходит после return).
+    * Т.е. произошел рендер с обновленным name, а после этого в предыдущее значение сохраняется name. */
+
+    console.log(prevName.current);
+
+    const handleSubmit = (text) => {
+        dispatch(setName(text));
+    };
+
+    return (
+        <>
+            <div className="profile">
+                <h3>This is Profile</h3>
+                <div>
+                    <input type="checkbox" id="checkbox" onClick={handleClick}></input>
+                    <label htmlFor="checkbox">show name</label>
+                </div>
+                {showName && <span>{name}</span>}
+                <Form onSubmit={handleSubmit} />
             </div>
-            {showName && <span>{name}</span>}
-            <FormContainer onSubmit={handleSubmit} />
-        </div>
-    </>
-);
+        </>
+    );
+}
