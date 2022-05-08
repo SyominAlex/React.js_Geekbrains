6080d04d2b35f96ce6d5f68c887b69bbfdff260c
SyominAlex <gurufx.43@gmail.com>
2022-05-09 00:02:40 +0300

    error capture

--- src/screens/Articles/Articles.js
+++ src/screens/Articles/Articles.js
@@ -3,16 +3,22 @@ import {useState} from "react";
 
 export const Articles = () => {
     const [articles, setArticles] = useState([]);
+    const [error, setError] = useState(null);
     const sendRequest = async () => {
         try {
             const response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error(`Response failed with status ${response.status}`);
+            }
             console.log('response', response);
 
             const result = await response.json();
             console.log('result', result);
             setArticles(result);
         } catch (e) {
-            console.log(e)}
+            console.log(e);
+            setError(e.message);
+        }
     };
 
     return (
@@ -20,6 +26,7 @@ export const Articles = () => {
             <div className="articles">
                 <h3>Articles</h3>
                 <button onClick={sendRequest}>Get</button>
+                {error && <h4>{error}</h4>}
                 <ul>
                     {articles.map((article) => (
                         <li key={article.id}>{article.title}</li>
