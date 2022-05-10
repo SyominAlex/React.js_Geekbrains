41ca2f66ba9623da99e51f6ae47372b45689f266
SyominAlex <gurufx.43@gmail.com>
2022-05-10 19:31:30 +0300

    full sync Profile (name, showName) with Realtime Database with Store (Redux)

--- src/screens/Profile/ProfileContainer.js
+++ src/screens/Profile/ProfileContainer.js
@@ -1,44 +1,32 @@
-import {useEffect, useState} from "react";
-// import {useDispatch} from "react-redux";
-// import {useSelector} from "react-redux";
-import {onValue, set} from "firebase/database";
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import {useSelector} from "react-redux";
 
-import {logOut, userNameRef, userShowNameRef} from "../../services/firebase";
-// import {toggleCheckbox} from "../../store/profile/actions";
-// import {setName} from "../../store/profile/actions";
-// import {selectName, selectShowName} from "../../store/profile/selectors";
+import {logOut} from "../../services/firebase";
+import {setShowName} from "../../store/profile/actions";
+import {setNameFB} from "../../store/profile/actions";
+import {selectName, selectShowName} from "../../store/profile/selectors";
 import {Profile} from "./Profile";
+import {initProfileTrack, stopProfileTrack} from "../../store/profile/actions";
 
 export const ProfileContainer = () => {
-    // const dispatch = useDispatch();
-    // const name = useSelector(selectName);
-    // const showName = useSelector(selectShowName);
-    const [name, setName] = useState('');
-    const [showName, setShowName] = useState(false);
+    const dispatch = useDispatch();
+    const name = useSelector(selectName);
+    const showName = useSelector(selectShowName);
 
     const handleClick = () => {
-        // dispatch(toggleCheckbox);
-        set(userShowNameRef, !showName);
+        dispatch(setShowName(!showName));
     };
 
     const handleSubmit = (text) => {
-        // dispatch(setName(text)); // меняет данные в сторе
-        // если передать null, объект будет удален в БД
-        set(userNameRef, text);
+        dispatch(setNameFB(text));
     };
 
     useEffect(() => {
-        const unsubscribeName = onValue(userNameRef, (snapshot) => {
-            setName(snapshot.val());
-        });
-
-        const unsubscribeShowName = onValue(userShowNameRef, (snapshot) => {
-            setShowName(snapshot.val());
-        });
+        dispatch(initProfileTrack());
 
         return () => {
-            unsubscribeName();
-            unsubscribeShowName();
+            dispatch(stopProfileTrack());
         };
     }, []);
 
