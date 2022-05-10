9123ff523fc6f92a73bddb7c99e17312dc699ccf
SyominAlex <gurufx.43@gmail.com>
2022-05-10 14:44:28 +0300

    half sync Profile (name, showName) with Realtime Database: work from DB to frontend

--- src/store/articles/actions.js
+++ src/store/articles/actions.js
@@ -22,27 +22,27 @@ export const getArticles = () => async (dispatch) => {
     try {
         dispatch(getArticlesRequest());
 
-        // const response = await fetch(apiUrl);
+        const response = await fetch(apiUrl);
         // try fetch with params from https://gbcdn.mrgcdn.ru/uploads/asset/3839580/attachment/091d1578a175eac2013778fc532b0b0f.pdf
-        const response = await fetch(apiUrl, {
-            method: 'GET',
-            _limit: 5, // parameter don't work https://api.spaceflightnewsapi.net/v3/documentation#/Article/get_articles
-            mode: 'cors',
-            cache: 'no-cache',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+        // const response = await fetch(apiUrl, {
+        //     method: 'GET',
+        //     _limit: 5, // parameter don't work https://api.spaceflightnewsapi.net/v3/documentation#/Article/get_articles
+        //     mode: 'cors',
+        //     cache: 'no-cache',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     }
+        // });
 
         if (!response.ok) {
             throw new Error(`Response failed with status ${response.status}`);
         }
-        console.log('response', response);
+        // console.log('response', response);
 
         const result = await response.json();
         dispatch(getArticlesSuccess(result));
     } catch (e) {
-        console.log(e);
+        // console.log(e);
         dispatch(getArticlesFailure(e.message));
     }
 };
