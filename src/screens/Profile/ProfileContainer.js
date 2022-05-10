fb4fd1e8d8a967f5d45f0be2acc5306b64ed8b0e
SyominAlex <gurufx.43@gmail.com>
2022-05-10 17:07:22 +0300

    full sync Profile (name, showName) with Realtime Database

--- src/screens/Profile/ProfileContainer.js
+++ src/screens/Profile/ProfileContainer.js
@@ -1,27 +1,36 @@
 import {useEffect, useState} from "react";
-import {useDispatch} from "react-redux";
+// import {useDispatch} from "react-redux";
 // import {useSelector} from "react-redux";
-import {onValue} from "firebase/database";
+import {onValue, set} from "firebase/database";
 
 import {logOut, userRef} from "../../services/firebase";
-import {toggleCheckbox} from "../../store/profile/actions";
+// import {toggleCheckbox} from "../../store/profile/actions";
 // import {setName} from "../../store/profile/actions";
 // import {selectName, selectShowName} from "../../store/profile/selectors";
 import {Profile} from "./Profile";
 
 export const ProfileContainer = () => {
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
     // const name = useSelector(selectName);
     // const showName = useSelector(selectShowName);
     const [name, setName] = useState('');
     const [showName, setShowName] = useState(false);
 
     const handleClick = () => {
-        dispatch(toggleCheckbox);
+        // dispatch(toggleCheckbox);
+        set(userRef, {
+            name,
+            showName: !showName,
+        });
     };
 
     const handleSubmit = (text) => {
-        dispatch(setName(text));
+        // dispatch(setName(text)); // меняет данные в сторе
+        // если передать null, объект будет удален в БД
+        set(userRef, {
+            name: text,
+            showName,
+        });
     };
 
     useEffect(() => {
